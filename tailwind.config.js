b50996f66cbbc9f45afd5e7cfa0636ee4b6d72a8
Vijayan <Nandhini.Vijayan@Honeywell.com>
2022-04-20 17:33:00 +0530

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      boxShadow:{
-        '2xl' : 'box-shadow: 0px 2px 20px #FFBCD1',
-      }
+      backgroundColor:{
+        'bg-mybg':'linear-gradient(180deg, #FFBCD1 0%, #FDEBF7 100%)',
+        },
+        boxShadow:
+        {
+          'forgot-div-password-shadow': '0px 4px 40px #A268AC',
+          'email-field-shadow' :'0px 2px 20px #BABABA',
+          'send-button-shadow':'0px 4px 20px 1px #FFBCD1'
+        }
     },
   },
   plugins: [],
