475ca5e2968afbecd6af9ce9a62667ccd3882694
Surya-the-coder <naansuryayadav@gmail.com>
2022-04-20 22:43:23 +0530

    API

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link'
 import '../assets/images/photo.png'
 
-const Card = () => {
+const Card = (props) => {
+  // const name = props.postData.id;
+  console.log("####################################################")
+  // let user = props.userData;
+  console.log(props.username);
   return (
     <Link href={'#'}>
       <div className='flex mx-5 my-5 bg-white py-2 px-5 rounded-3xl shadow-card'>
@@ -13,8 +17,8 @@ const Card = () => {
           </Link>
         </div>
         <div className=' pt-5'>
-          <h3 className='font-bold font-sans text-base'> Emma Sophia </h3>
-          <p className='text-xs text-purple-500'>01 January 2022</p>
+          <h3 className='font-bold font-sans text-base'> {props.username} </h3>
+          <p className='text-xs text-purple-500'>{props.createdData}</p>
           <p className='text-sm text-gray-500 mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui aenean iaculis bibendum lacus tincidunt arcu nunc lectus.</p>
           <div className='mt-3'>
             <Link href={'#'}>
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
 import TopBar from "../components/TopBar";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar";
 import Card from "../components/Card";
 
@@ -9,6 +9,19 @@ const home = () => {
     let [Recent, setRecent] = useState(false)
     let [Most, setMost] = useState(false)
     let [Top, setTop] = useState(false)
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+      getAllPosts();
+    }, [])
+    
+    let getAllPosts = async () => {
+        let postUrl = 'https://dream-pg-backend.herokuapp.com/api/post/';
+        let response = await fetch(postUrl);
+        let data = await response.json();
+        // console.log(data);
+        setPosts(data);
+    }
 
     let setAllFalse = () =>{
         setAll(false);
@@ -42,7 +55,7 @@ const home = () => {
     }
 
     return (
-        <div className="bg-pink-200 min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1]">
+        <div className="bg-pink-200 min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1] overflow-y-auto overflow-hidden">
             <meta name='theme-color' content='#FFBCD1' />
             <TopBar/>
             <div className="flex justify-around mx-10 top-24">
@@ -52,11 +65,11 @@ const home = () => {
                 <button className={Top?"bg-[#F67A95] text-white px-5 py-1 rounded-2xl" : " bg-white text-[#FF848E] px-5 py-1 rounded-2xl focus:bg-[#F67A95] focus:text-white"} onClick={() => pageSelected("Top")}>Top</button>
             </div>
             <p className="mx-8 my-8">Today</p>
-            <div className="overflow-y-auto">
+            <div className="">
                 <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {posts.map((post) =>
+                    <Card username = {post.user.username} createdData = {post.created_at}/>
+                )}
             </div>
 
             <NavBar/>
