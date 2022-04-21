27a474eaab2965086be0e28cf2909a416e22a0a6
Surya-the-coder <naansuryayadav@gmail.com>
2022-04-21 20:03:53 +0530

    Home page api usage for post details

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,11 +16,11 @@ const Card = (props) => {
         <div className=' pt-5'>
           <h3 className='font-Sarabun-Medium text-base tracking-[0.2px] font-medium'> {props.username} </h3>
           <p className='text-xs text-purple-500 font-Sarabun'>{props.createdData}</p>
-          <p className='text-sm text-gray-500 mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui aenean iaculis bibendum lacus tincidunt arcu nunc lectus.</p>
+          <p className='text-sm text-gray-500 mt-2'>{props.content}</p>
           <div className='mt-3 flex items-center'>
             <Reply></Reply>
             <Link href={'#'}>
-              <p className='text-xs mx-1'>4 replies</p>
+              <p className='text-xs mx-1'>{props.numberOfLikes}</p>
             </Link>
           </div>
         </div>
