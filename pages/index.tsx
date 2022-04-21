24d00a3f84d1263cd4423bfff174f0cea18ec6a0
Vijayan <Nandhini.Vijayan@Honeywell.com>
2022-04-21 17:25:09 +0530

    HomePage fit check

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,16 +13,16 @@ const Home: NextPage = () => {
       <div className='w-full flex items-center justify-center md:hidden'>
         <Vector className="w-full"></Vector>
       </div>
-      <div className="mt-4">
-        <h2 className="pt-6 text-center font-[segoepr] text-3xl font-bold">Welcome!</h2>
-        <p className=" pt-1 text-center font-[Sarabun-SemiBold] text-xs font-semibold text-[#939090]">Create a new account</p>
+      <div className="mt-0">
+        <h2 className="text-center font-[segoepr] text-3xl font-bold">Welcome!</h2>
+        <p className="pt-1 text-center font-[Sarabun-SemiBold] text-xs font-semibold text-[#939090]">Create a new account</p>
       </div>
-      <div className="mt-11 flex flex-col items-center w-full">
+      <div className="mt-5 flex flex-col items-center w-full">
         
         <input className=" focus-welcome-field-shadowfocus h-14 w-80 rounded-2xl border pl-6 font-[Sarabun-SemiBold] text-xs font-semibold shadow-welcome-field-shadowbefore focus:border-2 focus:border-[#FFBCD1] focus:outline-none focus:placeholder:text-[#FFBCD1]" type="text" name="Username" id="username" placeholder="Username"/>
-        <input className=" focus-welcome-field-shadowfocus mt-8 h-14 w-80 rounded-2xl border pl-6 font-[Sarabun-SemiBold] text-xs font-semibold shadow-welcome-field-shadowbefore focus:border-2 focus:border-[#FFBCD1] focus:outline-none focus:placeholder:text-[#FFBCD1]" type="password" name="Password" id="password" placeholder="Password"/>
+        <input className=" focus-welcome-field-shadowfocus mt-4 h-14 w-80 rounded-2xl border pl-6 font-[Sarabun-SemiBold] text-xs font-semibold shadow-welcome-field-shadowbefore focus:border-2 focus:border-[#FFBCD1] focus:outline-none focus:placeholder:text-[#FFBCD1]" type="password" name="Password" id="password" placeholder="Password"/>
         
-        <div className=" px-2 mt-4 flex w-full max-w-md items-center justify-between">
+        <div className=" px-2 mt-3 flex w-full max-w-md items-center justify-between">
           <div className="mx-10 flex items-center justify-center">
             <input className=" accent-pink-500 outline-hidden h-4 w-4 " type="radio" id="radiobutton" name="radiobutton"/>
             <label className="pl-2 font-[Sarabun-ExtraBold] text-xs font-extrabold"> Remember Me </label>
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
             <p className="font-[Sarabun-ExtraBold] text-xs font-extrabold "> Forgot password? </p>
           </div>          
         </div>
-        <button className=' mt-20 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#F67A95] rounded-full w-64 h-16'>Sign In</button>  
+        <button className=' mt-10 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#F67A95] rounded-full w-64 h-16'>Sign In</button>  
       </div>
       <div className='w-full flex items-center justify-center md:hidden bottom-0'>
           <FooterVector className="w-full">
