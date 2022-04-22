697f1149b62daf3b459763dc3577ff6aebbefa44
Sundararajan <Surya.Sundararajan@Honeywell.com>
2022-04-22 14:06:23 +0530

    homepage back icon route

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -3,12 +3,15 @@ import BellIcon from '../assets/images/Bell.svg';
 import Search from '../assets/images/Search.svg';
 import Profile from '../assets/images/Profile.svg';
 import BackButton from '../assets/images/Shape.svg';
+import Link from 'next/link';
 
 const TopBar = () => {
   return (
     <div className="flex py-10 justify-between items-center">
       <div className="flex mx-2 justify-center items-center">
-        <BackButton className="mx-2"></BackButton>
+        <Link href={'/'}>
+          <BackButton className="mx-2"></BackButton>
+        </Link> 
         <Profile className="mx-2"/>
         <h4 className='mx-2 text-[#A268AC] font-Sarabun-SemiBold'>User_name</h4>
       </div>
