a0224aa724a3f7073971334f8f3bbc0d0ffd5663
Vijayan <Nandhini.Vijayan@Honeywell.com>
2022-04-24 20:53:35 +0530

    Updated Code By Nandy

---
+++ components/AccountDetailsTopBar.tsx
@@ -0,0 +1,26 @@
+import BellIcon from '../assets/images/Bell.svg';
+import Search from '../assets/images/Search.svg';
+import Profile from '../assets/images/Profile.svg';
+import { url } from 'inspector';
+
+const AccountDetailsTopBar = () => {
+	return (
+		<div className='flex flex-col w-full max-w-md'>
+			<div className="flex mt-3 justify-between items-center">     			 
+				<p className='mx-4 text-[#5F5D5D] font-[Sarabun-SemiBold[ font-semibold text-xl'>Account Details</p> 				
+				<div className="mx-4 flex">
+          			<BellIcon className="mx-2"/>
+          			<Search className="mx-2"/>
+      			</div>
+			</div>
+			<div className='pt-2 flex flex-col justify-center items-center'>
+				<img src={'https://ucarecdn.com/05f649bf-b70b-4cf8-90f7-2588ce404a08/'} className=' rounded-full h-16 w-16'></img> {/* <Profile className="mx-2"/> */}
+        		<h4 className='mx-2 text-[#A268AC] font-[Sarabun-SemiBold] font-semibold text-'>User_name</h4>       
+			</div>				
+      	</div>
+
+     
+	);
+}
+
+export default AccountDetailsTopBar;
--- pages/changepassword.tsx
+++ pages/changepassword.tsx
@@ -1,10 +1,20 @@
+import Head from 'next/head';
+import AccountDetailsTopBar from '../components/AccountDetailsTopBar'
 const changepassword = () => {
 	return (
-		<div>
-			<p>Enter</p>
+		// <div className='min-h-screen w-full flex flex-col justify-center items-center'>
+		// 	<h1>Sample</h1>
+		// </div>
+		<div className=" flex flex-col items-center min-h-screen w-full bg-pink-300">
+			<Head>
+				<meta name='theme-color' content='#FFBCD1' />
+			</Head>
+			<AccountDetailsTopBar/>
+			<div className='mx-6 h-[65vh] max-h-[70vh] w-[95vw] max-w-md rounded-3xl mt-7 bg-[#FFFFFF] flex justify-center'>
+				<input className=" text-[#B9B9B9] focus-welcome-field-shadowfocus pl-6 mt-10  rounded-2xl border w-80 h-14 mx-3 font-[Sarabun-SemiBold] text-xs font-semibold shadow-welcome-field-shadowbefore focus:border-2 focus:border-[#FFBCD1] focus:outline-none focus:placeholder:text-[#FFBCD1]" type="email" name="email" id="email" placeholder="Email"/>
+			</div>
 		</div>
 	);
-
 }
 
 export default changepassword;
