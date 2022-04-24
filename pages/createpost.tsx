110feafaa3fc66be6ab4cb30bb9f08348709982e
Surya-the-coder <naansuryayadav@gmail.com>
2022-04-24 21:21:24 +0530

    making createpost protected

--- pages/createpost.tsx
+++ pages/createpost.tsx
@@ -1,25 +1,41 @@
 import TopBar from '../components/TopBar'
 import NavBar from '../components/NavBar'
-const createpost = () => {
-	return (
-		<div className=" min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1]">
-		<div className="">
-			<meta name='theme-color' content='#FFBCD1' />
-			<TopBar/>	
-			<div className='flex mx-6'>
-				<p className='font-[Sarabun-SemiBold] text-semibold text-[#2F2F2F] text-xl'>Create Post</p>			
+import { getSession } from 'next-auth/react';
+import Head from 'next/head';
+
+const createpost = ({session}) => {
+	if (session) {
+		return (
+			<div className="flex justify-center min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1]">
+				<div className="flex flex-col max-w-md">
+					<Head>
+						<meta name='theme-color' content='#FFBCD1' />
+					</Head>
+					<TopBar/>
+					<div className='flex mx-6'>
+						<p className='font-[Sarabun-SemiBold] text-semibold text-[#2F2F2F] text-xl'>Create Post</p>			
+					</div>
+					<div className=' h-[35vh] max-h-[40vh] mx-6 mt-5 justify-center'>
+						<textarea className='  shadow-welcome-field-shadowfocus pl-5 pt-8 rounded-xl min-h-full min-w-full max-h-[40vh]' id="caption" placeholder='write a caption..'/>	
+						<div className='flex'>
+							<button className=' h-16 w-40 mt-5 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#C1C1C1] rounded-3xl'>Back</button>  
+							<button className=' ml-8 h-16 w-40 mt-5 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#F67A95] rounded-3xl'>Post</button> 
+						</div>
+					</div>
+				</div>
+				<NavBar/>
 			</div>
-			<div className=' h-[35vh] max-h-[40vh] mx-6 mt-5 justify-center'>
-				<textarea className='  shadow-welcome-field-shadowfocus pl-5 pt-8 rounded-xl min-h-full min-w-full ' id="caption" placeholder='write a caption..'/>	
-			</div>
-			<div className='flex mx-7 items-center justify-center'>
-				<button className=' h-16 w-40 mt-5 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#C1C1C1] rounded-3xl'>Back</button>  
-				<button className=' ml-8 h-16 w-40 mt-5 text-white shadow-button-shadow font-[Sarabun-Regular] font-normal -tracking-tighter bg-[#F67A95] rounded-3xl'>Post</button> 
-			</div>
-		</div>
-		<NavBar/>
-		</div>
-	);
+		);
+	}
 }
 
 export default createpost;
+
+
+export async function getServerSideProps (context) {
+    const session = await getSession(context);
+    if (!session) {
+        return{redirect :{destination: '/', permanent : false}}
+    }
+    return {props : {session}}
+  }
