cb5b34f395d17c1c4eb198cb31e62e37a33710ff
vmtn2 <sathrt@gmail.com>
2022-07-01 00:20:05 +0530

    Look_10

--- pages/post/[postid].tsx
+++ pages/post/[postid].tsx
@@ -84,7 +84,7 @@ const SinglePost = () => {
         console.log('Setting is Data Fetched to true in getSinglePostData')
         setIsDataFetched(true);
         gsap.from(".single-card", {
-            y:30, rotationY:45,ease: "back.out(1.6)",opacity:0.6,duration:3,
+            y:30, scale:0.6, ease: "back.out(1.6)",opacity:0.6,duration:1.5,
         })
     }
     if (accessToken!=null) {
