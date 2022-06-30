36e22e8db046de3a7cdbad6e71113db09846a8b6
vmtn2 <sathrt@gmail.com>
2022-06-30 23:26:21 +0530

    Look_2

--- pages/home.tsx
+++ pages/home.tsx
@@ -37,7 +37,7 @@ const addAnimations = (cardRef) => {
             }
         })
         gsap.from(cardRef.current[i], {
-            x: -70,y:0,duration:0.3,ease: "expo.out",
+            x: -10,y:0,duration:0.3,ease: "back.out(1.7)",
             scrollTrigger: {
                 trigger: cardRef.current[i],
                 toggleActions: "restart none none reset",
