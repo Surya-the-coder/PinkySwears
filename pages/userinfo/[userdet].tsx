5287c1fa7f9b1c896d4dbd4dfaba4d4c434f0e50
vmtn2 <sathrt@gmail.com>
2022-08-03 21:04:55 +0530

    User Details page Working - realigned

--- pages/userinfo/[userdet].tsx
+++ pages/userinfo/[userdet].tsx
@@ -10,7 +10,6 @@ import {getFollowing,getFollowers,isEmptyObject} from '../../components/CommonFu
 
 
 let postUserInfo
-// let postsOfUser
 let postsData
 let followerCount
 let followingCount
@@ -67,31 +66,27 @@ const userdet = () => {
 				}
 				setIsDataFetched(true)
 			})
-
-
 		}
 	},[reRender])
 
     let getAllPostsOfUser = async () => {
         let fetchAllPostApiUrl = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/post/user/${router.query.userdet}/`;
-		// console.log(router.query.userdet + " user id")
-			let response = await fetch(fetchAllPostApiUrl);
-			if (response.ok) {
-				let this_postData = await response.json()
+		let response = await fetch(fetchAllPostApiUrl);
+		if (response.ok) {
+			let this_postData = await response.json()
 
-				if (this_postData.length > 0) {
-					postsData = this_postData
-					// setPostsData(postData)
-					// setPostsOfUser(Object.keys(postData).length);
-					setPostsOfUser(Object.keys(this_postData).length)
-					console.log(postsOfUser)
+			if (this_postData.length > 0) {
+				postsData = this_postData
+				// setPostsData(postData)
+				// setPostsOfUser(Object.keys(postData).length);
+				setPostsOfUser(Object.keys(this_postData).length)
+				console.log(postsOfUser)
 
-				} else {
+			} else {
 
-					setPostsOfUser(0)
-				}
-				// setReRender(!reRender)
+				setPostsOfUser(0)
 			}
+		}
     }
 
 	let getUserInfo = async () => {
@@ -107,14 +102,9 @@ const userdet = () => {
 		if (response.ok) {
 			let this_userData = await response.json()
 			postUserInfo = this_userData
-			console.log(postUserInfo)
-			// console.log(postUserInfo.last_name)
-			// setPostUserInfo(userData)
-
 		}
 		else
 		{
-
 			// setUserNotFound(true)
 			// setIsDataFetched(true)
 		}
@@ -150,8 +140,6 @@ const userdet = () => {
 		let followinginfojson = getFollowing(router.query.userdet)
 		followinginfojson.then(async function(result){
 			if (result!==null) {
-				// console.log(result)
-				// console.log(Object.keys(result).length)
 				// setFollowingCount(await Object.keys(result).length);
 				followingCount = Object.keys(result).length
 			}
@@ -165,95 +153,87 @@ const userdet = () => {
 
 	}
 	let followUnFollowUser =async () => {
-		// console.log("Followedd.................")
-		// console.log("Follow Function")
-		let response= await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/user/follow/${router.query.userdet}/`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					'Authorization': 'Bearer '+accessToken,
-				},
-			});
-			// console.log(response)
-			let procesinfo = await response.json()
-			// console.log(procesinfo.processdone)
-		if(response.status==202)
-		{
-			if(procesinfo.processdone=="unfollowed")
-				console.log("User Unfollowed Successfully")
-				if(procesinfo.processdone=="followed")
-				console.log("User Followed Successfully")
-		}
-		// console.log("State Change")
-		// console.log(isFollowing)
-		isFollowing = !isFollowing
-		// setFollow(!isFollowing)
+	let response= await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/user/follow/${router.query.userdet}/`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				'Authorization': 'Bearer '+accessToken,
+			},
+		});
+	let procesinfo = await response.json()
+	if(response.status==202)
+	{
+		if(procesinfo.processdone=="unfollowed")
+			console.log("User Unfollowed Successfully")
+			if(procesinfo.processdone=="followed")
+			console.log("User Followed Successfully")
+	}
+	// console.log("State Change")
+	// console.log(isFollowing)
+	isFollowing = !isFollowing
+	// setFollow(!isFollowing)
 	}
 
 
 	if(accessToken!=null)
 	{
-		console.log('Rendering ')
-		console.log(isDataFetched + "isDataFetched")
-
 		return (
 			<div className="flex flex-col bg-pink-200 min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1] w-full">
     	        <Head>
 				   <meta name='theme-color' content='#FFBCD1' />
 				</Head>
 				<TopBar backButton = {true}/>
-				{/*{console.log(postUserInfo.profileImg)}*/}
-				{isDataFetched ? <div>
-						{console.log(postUserInfo)}
-					{console.log(postsOfUser + " posts of user")}
+
+				{isDataFetched ?
+					<div>
 						<div className="flex items-center justify-center">
-									<div className=" h-[84px] w-[84px]">
-										<img
-											src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/` + `${postUserInfo.profileImg !== null ? postUserInfo.profileImg : '/media/userDefault.jpg'}`}
-											className='w-full h-full rounded-full'/>
-									</div>
-									<div className=" ml-[40px] flex flex-col">
-										<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Posts</p>
-										<p className=" text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000] ">{postsOfUser}</p>
-									</div>
-									<div className="ml-[16px] flex flex-col">
-										<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Following</p>
-										<p className=" text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000]">{followingCount}</p>
-									</div>
-									<div className=" ml-[16px] flex flex-col">
-										<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Followers</p>
-										<p className="text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000]">{followerCount}</p>
-									</div>
-								</div>
-								<div className="  ml-[40px] mt-[13px] flex flex-col  justify-center">
-									<p className=" font-[Sarabun-Medium] font-semibold text-[#939090] text-sm">@{postUserInfo.username}</p>
-									<h5 className="font-[Sarabun-Medium] font-semibold text-black text-sm  mt-[3px]">{postUserInfo.first_name} {postUserInfo.last_name}</h5>
-									{/* <h6 className=" font-[Sarabun-Medium] font-semibold text-black text-xs mt-[7px]">Programmer, developer, designer...</h6> */}
-								</div>
-
-								{selfView ?
-									null
-									:
-									<div className="mx-[40px] mt-[19px] flex items-center justify-center ">
-										<button
-											className=" rounded-3xl w-[201px] h-[45px] font-[Sarabun-Medium] font-semibold text-sm text-[#ffffff] bg-[#F67A95]"
-											onClick={followUnFollowUser}> {isFollowing ? "Unfollow" : "Follow"} </button>
-									</div>
-								}
-
-								<div className="">
-									{postsOfUser>0?
-										postsData.map((post) =>
-										<Card key={post.id} accessToken={accessToken} postid={post.id}
-											  userid={post.user.id} setClickedCard={setClickedCard}
-											  username={post.user.first_name + ' ' + post.user.last_name}
-											  profileImage={post.user.profileImg} content={post.content}
-											  createdData={dateFormat(post.created_at, "dS mmmm yyyy")}
-											  numberOfLikes={post.likes} commentsCount={post.comments_count}/>
-
-									):null}
-								</div>
+							<div className=" h-[84px] w-[84px]">
+								<img
+									src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/` + `${postUserInfo.profileImg !== null ? postUserInfo.profileImg : '/media/userDefault.jpg'}`}
+									className='w-full h-full rounded-full'/>
 							</div>
+							<div className=" ml-[40px] flex flex-col">
+								<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Posts</p>
+								<p className=" text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000] ">{postsOfUser}</p>
+							</div>
+							<div className="ml-[16px] flex flex-col">
+								<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Following</p>
+								<p className=" text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000]">{followingCount}</p>
+							</div>
+							<div className=" ml-[16px] flex flex-col">
+								<p className=" font-[Sarabun-Medium] font-semibold text-xs text-[#A268AC]">Followers</p>
+								<p className="text-center mt-[8px] font-[Sarabun] font-bold text-xs text-[#000000]">{followerCount}</p>
+							</div>
+						</div>
+						<div className="  ml-[40px] mt-[13px] flex flex-col  justify-center">
+							<p className=" font-[Sarabun-Medium] font-semibold text-[#939090] text-sm">@{postUserInfo.username}</p>
+							<h5 className="font-[Sarabun-Medium] font-semibold text-black text-sm  mt-[3px]">{postUserInfo.first_name} {postUserInfo.last_name}</h5>
+							{/* <h6 className=" font-[Sarabun-Medium] font-semibold text-black text-xs mt-[7px]">Programmer, developer, designer...</h6> */}
+						</div>
+
+						{selfView ?
+							null
+							:
+							<div className="mx-[40px] mt-[19px] flex items-center justify-center ">
+								<button
+									className=" rounded-3xl w-[201px] h-[45px] font-[Sarabun-Medium] font-semibold text-sm text-[#ffffff] bg-[#F67A95]"
+									onClick={followUnFollowUser}> {isFollowing ? "Unfollow" : "Follow"} </button>
+							</div>
+						}
+
+						<div className="">
+							{postsOfUser>0?
+								postsData.map((post) =>
+								<Card key={post.id} accessToken={accessToken} postid={post.id}
+									  userid={post.user.id} setClickedCard={setClickedCard}
+									  username={post.user.first_name + ' ' + post.user.last_name}
+									  profileImage={post.user.profileImg} content={post.content}
+									  createdData={dateFormat(post.created_at, "dS mmmm yyyy")}
+									  numberOfLikes={post.likes} commentsCount={post.comments_count}/>
+
+							):null}
+						</div>
+					</div>
 					:
 					<LoadingSpinner/>
 
