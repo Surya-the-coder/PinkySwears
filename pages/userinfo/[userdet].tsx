2294be3653a5b6a05165e18b525c2429b590bfa1
vmtn2 <sathrt@gmail.com>
2022-08-04 09:12:42 +0530

    User Details Page - Correct user loads only on reload - Needs fixing

--- pages/userinfo/[userdet].tsx
+++ pages/userinfo/[userdet].tsx
@@ -6,16 +6,33 @@ import AccountCard  from "../../components/AccountCard";
 import Card  from "../../components/Card";
 import dateFormat from 'dateformat';
 import LoadingSpinner from "../../components/LoadingSpinner";
-import {getFollowing,getFollowers,isEmptyObject} from '../../components/CommonFunctions'
+import {getFollowing, getFollowers, isEmptyObject, isAccessTokenValid} from '../../components/CommonFunctions'
+import {gsap} from "gsap";
+const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
+const { ScrollToPlugin } = require("gsap/dist/ScrollToPlugin");
 
 
-let postUserInfo
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+
+// let postUserInfo
 let postsData
-let followerCount
-let followingCount
+// let followerCount
+// let followingCount
 let refreshToken
-let isFollowing
-let selfView
+// let isFollowing
+// let selfView
+
+const scrollToCard = () => {
+		console.log('Scrolling to card')
+		const scrollDiv = `#card-${sessionStorage.getItem('clickedCard')}`
+		console.log(scrollDiv)
+		gsap.to(window, {scrollTo: scrollDiv}).then(() => {
+			gsap.from(scrollDiv, {duration: 1, backgroundColor: "yellow"})
+			sessionStorage.setItem('clickedCard', '')
+		})
+
+}
 
 const userdet = () => {
 
@@ -24,6 +41,11 @@ const userdet = () => {
 	const [isDataFetched, setIsDataFetched] = useState(false)
 	const [clickedCard,setClickedCard] = useState<any>()
 	const [postsOfUser,setPostsOfUser] = useState<any>()
+	const [followerCount,setFollowerCount] = useState<any>()
+	const [followingCount,setFollowingCount] = useState<any>()
+	const [isFollowing,setIsFollowing] = useState<any>()
+	const [selfView,setSelfView] = useState<boolean>(false)
+	const [postUserInfo,setPostUserInfo] = useState<any>()
 	const [reRender,setReRender] = useState(false)
 
 
@@ -35,39 +57,30 @@ const userdet = () => {
         if (accessTokenLS == null) {
             router.push('/')
         }
-        else{
-            setaccessToken(accessTokenLS)
-            refreshToken = refreshTokenLS
-			if (router.isReady) {
-				getUserInfo().then(() => {
-						getAllPostsOfUser();
-						callCommonFunctions()
-						if (userID == router.query.userdet) {
-							selfView = true
-						}
-					setIsDataFetched(true)
-				})
-				setReRender(!reRender)
-
-
-			}
-        }
-    }, [router.isReady]);
-
-	useEffect(() => {
-		let userID = JSON.parse(localStorage.getItem('UserDetails')).id
-
-		if (router.isReady) {
-			getUserInfo().then(() => {
-				getAllPostsOfUser();
-				callCommonFunctions()
-				if (userID == router.query.userdet) {
-					selfView = true
+        else {
+			if (isAccessTokenValid(accessTokenLS, refreshTokenLS)) {
+				setaccessToken(accessTokenLS)
+				refreshToken = refreshTokenLS
+				if (router.isReady) {
+					getUserInfo().then(() => {
+						getAllPostsOfUser().then(() => {
+							callCommonFunctions().then(() => {
+								if (userID == router.query.userdet) {
+									setSelfView(true)
+								}
+								setIsDataFetched(true)
+								scrollToCard()
+							})
+						})
+					})
 				}
-				setIsDataFetched(true)
-			})
+			}
+			else {
+				console.log('No Access Token')
+				router.push('/')
+			}
 		}
-	},[reRender])
+    }, [router.isReady]);
 
     let getAllPostsOfUser = async () => {
         let fetchAllPostApiUrl = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/post/user/${router.query.userdet}/`;
@@ -80,10 +93,8 @@ const userdet = () => {
 				// setPostsData(postData)
 				// setPostsOfUser(Object.keys(postData).length);
 				setPostsOfUser(Object.keys(this_postData).length)
-				console.log(postsOfUser)
-
-			} else {
-
+			}
+			else {
 				setPostsOfUser(0)
 			}
 		}
@@ -101,10 +112,12 @@ const userdet = () => {
 		});
 		if (response.ok) {
 			let this_userData = await response.json()
-			postUserInfo = this_userData
+			setPostUserInfo(this_userData)
+			console.log(this_userData)
 		}
 		else
 		{
+			console.log(response)
 			// setUserNotFound(true)
 			// setIsDataFetched(true)
 		}
@@ -116,38 +129,33 @@ const userdet = () => {
 		followerinfojson.then(async function (result) {
 			if (result!==null) {
 				// setFollowerCount(Object.keys(result).length);
-				followerCount = Object.keys(result).length
+				setFollowerCount(Object.keys(result).length)
+				console.log(followerCount)
 				var newA = result.filter(function (item) {
 					return item.id == userId;
 				});
 				const countOfArray = (Object.keys(newA).length)
 				if (countOfArray == 0)
-					isFollowing = false
-					// setFollow(false)
+					setIsFollowing(false)
 				else
-					isFollowing = true
-					// setFollow(true)
+					setIsFollowing(true)
 			}
 			else
 			{
-				followerCount = 0
-				// setFollowerCount(0)
-				isFollowing = false
-				// setFollow(false)
+
+				setFollowerCount(0)
+				setIsFollowing(false)
 			}
 		})
 
 		let followinginfojson = getFollowing(router.query.userdet)
 		followinginfojson.then(async function(result){
 			if (result!==null) {
-				// setFollowingCount(await Object.keys(result).length);
-				followingCount = Object.keys(result).length
+				setFollowingCount(await Object.keys(result).length);
 			}
 			else
 			{
-
-				// setFollowingCount(0)
-				followingCount = 0
+				setFollowingCount(0)
 			}
 		})
 
@@ -170,22 +178,22 @@ const userdet = () => {
 	}
 	// console.log("State Change")
 	// console.log(isFollowing)
-	isFollowing = !isFollowing
-	// setFollow(!isFollowing)
+	setIsFollowing(!isFollowing)
 	}
 
 
 	if(accessToken!=null)
 	{
 		return (
-			<div className="flex flex-col bg-pink-200 min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1] w-full">
+			<div id='scrollParent' className="flex flex-col bg-pink-200 min-h-screen bg-gradient-to-t from-[#FDEBF7] to-[#FFBCD1] w-full">
     	        <Head>
 				   <meta name='theme-color' content='#FFBCD1' />
 				</Head>
 				<TopBar backButton = {true}/>
 
 				{isDataFetched ?
-					<div>
+
+					<div>{console.log(followerCount)}
 						<div className="flex items-center justify-center">
 							<div className=" h-[84px] w-[84px]">
 								<img
@@ -225,11 +233,13 @@ const userdet = () => {
 							{postsOfUser>0?
 								postsData.map((post) =>
 								<Card key={post.id} accessToken={accessToken} postid={post.id}
-									  userid={post.user.id} setClickedCard={setClickedCard}
+									  userid={post.user.id}
+									  setClickedCard={setClickedCard}
 									  username={post.user.first_name + ' ' + post.user.last_name}
 									  profileImage={post.user.profileImg} content={post.content}
 									  createdData={dateFormat(post.created_at, "dS mmmm yyyy")}
-									  numberOfLikes={post.likes} commentsCount={post.comments_count}/>
+									  numberOfLikes={post.likes} commentsCount={post.comments_count}
+								/>
 
 							):null}
 						</div>
